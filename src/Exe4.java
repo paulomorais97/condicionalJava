b96ba9d0991d072fa501d36cc5f0802d44bd3e1f
paulomorais97 <pauli-2109@hotmail.com>
2020-09-09 15:42:56 -0300

    atualizacao condicional

--- src/Exe4.java
+++ src/Exe4.java
@@ -3,12 +3,7 @@ import java.util.Random;
 public class Exe4 {
 
 	public static void main(String[] args) {
-		/*
-		 * Faça um programa em que permita a entrada de um 
-		 * número qualquer e exiba se este número é par ou ímpar.
-		 *  Se for par exiba também a raiz quadrada do mesmo; 
-		 *  se for ímpar exiba o número elevado ao quadrado.
-		 */
+		
 		Random r = new Random();
 		
 		int numero;
